be7b075cbbe8bb2a7b0d6225a213a36da6f7a302
Alisher <194268626+alisheryeginbay@users.noreply.github.com>
2025-10-22 06:31:06 +0400

    style: biome format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,25 @@
-// @ts-check
-import { defineConfig, fontProviders } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yeginbay.com",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.google(),
-        name: "Spectral",
-        cssVariable: "--font-spectral",
-      },
-    ],
-  },
+	site: "https://yeginbay.com",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.google(),
+				name: "Spectral",
+				cssVariable: "--font-spectral",
+			},
+		],
+	},
 });
